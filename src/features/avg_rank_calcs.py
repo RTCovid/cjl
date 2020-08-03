510733f95174a10d63c2510171a4ab7087c2cccb
alexmerryman <alexandermerryman2018@u.northwestern.edu>
2020-08-03 14:11:32 -0600

    - Added process to re-rank the category overall rank averages. - Renamed overall rank average fields to include the broad metric category (health, crime, housing_env, edu). - Added more To-Dos.

--- src/features/avg_rank_calcs.py
+++ src/features/avg_rank_calcs.py
@@ -8,9 +8,9 @@ sys.path.append("../")
 
 from config import *
 
+# TODO: Modularize the below into function(s)
 zip_data_loc = os.path.join(INTERIM_DATA_DIR, "zip_level_metrics.csv")
 zip_df = pd.read_csv(zip_data_loc)
-
 
 PRIMARY_KEY_COLS = ['Zip Code']
 raw_metric_cols = [
@@ -84,11 +84,9 @@ crime_df = zip_df[[*PRIMARY_KEY_COLS, *CRIME_METRICS]]
 housing_env_df = zip_df[[*PRIMARY_KEY_COLS, *HOUSING_ENVIRONMENT_METRICS]]
 educ_df = zip_df[[*PRIMARY_KEY_COLS, *EDUCATION_METRICS]]
 
-educ_df.describe()
-housing_env_df.describe()
-health_df.describe()
-len(HOUSING_ENVIRONMENT_METRICS)
-print(educ_df.head())
+# educ_df.describe()
+# housing_env_df.describe()
+# health_df.describe()
 
 
 def clean_zip_codes(df):
@@ -104,7 +102,10 @@ def clean_zip_codes(df):
     return df
 
 
-def avg_rank_pctile(df):
+def avg_rank_pctile(df, metric_category):
+
+    # TODO: Drop records (zip codes) which are missing ALL (or some?) metrics
+
     for c in df.columns:
         if c != 'Zip Code':
             df[f"{c}_dense_rank"] = df[c].rank(method="dense", na_option="bottom")  # TODO: What direction (asc/desc) should each metric be ranked? E.g. are values "better" if higher or lower
@@ -121,8 +122,20 @@ def avg_rank_pctile(df):
         return statistics.mean(dense_rank_vals), statistics.mean(dense_rank_pctile_vals), \
                statistics.mean(avg_rank_vals), statistics.mean(avg_rank_pctile_vals)
 
-    df['overall_avg_dense_rank'], df['overall_avg_dense_rank_pctile'], \
-    df['overall_avg_avg_rank'], df['overall_avg_avg_rank_pctile'] = zip(*df.apply(lambda row: avg_vals_row(row), axis=1))
+    df[f'overall_avg_dense_rank_{metric_category}'], df[f'overall_avg_dense_rank_pctile_{metric_category}'], \
+    df[f'overall_avg_avg_rank_{metric_category}'], df[f'overall_avg_avg_rank_pctile_{metric_category}'] = zip(*df.apply(lambda row: avg_vals_row(row), axis=1))
+
+    # TODO: Re-merge (outer join) ranked records with all original zip codes (before dropping those missing all metrics)
+    # TODO: Fill unranked zip codes with NaN
+
+    return df
+
+
+def re_rank_overall_avgs(df, metric_category):
+    df[f"overall_avg_dense_rank_RANK_{metric_category}"] = df[f'overall_avg_dense_rank_{metric_category}'].rank(method="dense", na_option="bottom")
+    df[f"overall_avg_dense_rank_pctile_RANK_{metric_category}"] = df[f'overall_avg_dense_rank_pctile_{metric_category}'].rank(method="dense", na_option="bottom")
+    df[f"overall_avg_avg_rank_RANK_{metric_category}"] = df[f'overall_avg_avg_rank_{metric_category}'].rank(method="dense", na_option="bottom")
+    df[f"overall_avg_avg_rank_pctile_RANK_{metric_category}"] = df[f'overall_avg_avg_rank_pctile_{metric_category}'].rank(method="dense", na_option="bottom")
 
     return df
 
@@ -133,17 +146,29 @@ def main():
     housing_env_df_cleaned = clean_zip_codes(housing_env_df)
     educ_df_cleaned = clean_zip_codes(educ_df)
 
-    health_df_cleaned_ranked = avg_rank_pctile(health_df_cleaned)
-    health_df_cleaned_ranked.to_csv(os.path.join(PROCESSED_DATA_DIR, 'health_df_cleaned_ranked.csv'), index=False)
+    health_df_cleaned_ranked = avg_rank_pctile(health_df_cleaned, 'health')
+    crime_df_cleaned_ranked = avg_rank_pctile(crime_df_cleaned, 'crime')
+    housing_env_df_cleaned_ranked = avg_rank_pctile(housing_env_df_cleaned, 'housing_env')
+    educ_df_cleaned_ranked = avg_rank_pctile(educ_df_cleaned, 'edu')
 
-    crime_df_cleaned_ranked = avg_rank_pctile(crime_df_cleaned)
-    crime_df_cleaned_ranked.to_csv(os.path.join(PROCESSED_DATA_DIR, 'crime_df_cleaned_ranked.csv'), index=False)
+    health_df_cleaned_re_ranked = re_rank_overall_avgs(health_df_cleaned_ranked, 'health')
+    crime_df_cleaned_re_ranked = re_rank_overall_avgs(crime_df_cleaned_ranked, 'crime')
+    housing_env_df_cleaned_re_ranked = re_rank_overall_avgs(housing_env_df_cleaned_ranked, 'housing_env')
+    educ_df_cleaned_re_ranked = re_rank_overall_avgs(educ_df_cleaned_ranked, 'edu')
 
-    housing_env_df_cleaned_ranked = avg_rank_pctile(housing_env_df_cleaned)
-    housing_env_df_cleaned_ranked.to_csv(os.path.join(PROCESSED_DATA_DIR, 'housing_env_df_cleaned_ranked.csv'), index=False)
+    # health_df_cleaned_re_ranked.to_csv(os.path.join(PROCESSED_DATA_DIR, 'health_df_cleaned_ranked.csv'), index=False)
+    # crime_df_cleaned_re_ranked.to_csv(os.path.join(PROCESSED_DATA_DIR, 'crime_df_cleaned_ranked.csv'), index=False)
+    # housing_env_df_cleaned_re_ranked.to_csv(os.path.join(PROCESSED_DATA_DIR, 'housing_env_df_cleaned_ranked.csv'), index=False)
+    # educ_df_cleaned_re_ranked.to_csv(os.path.join(PROCESSED_DATA_DIR, 'educ_df_cleaned_ranked.csv'), index=False)
 
-    educ_df_cleaned_ranked = avg_rank_pctile(educ_df_cleaned)
-    educ_df_cleaned_ranked.to_csv(os.path.join(PROCESSED_DATA_DIR, 'educ_df_cleaned_ranked.csv'), index=False)
+    # dfs = [health_df_cleaned_re_ranked, crime_df_cleaned_re_ranked, housing_env_df_cleaned_re_ranked, educ_df_cleaned_re_ranked]
+    # final_joined_df = pd.concat(dfs, axis=1, join='outer')
+
+    merge1 = pd.merge(left=health_df_cleaned_re_ranked, right=crime_df_cleaned_re_ranked, how='outer', on='Zip Code')
+    merge2 = pd.merge(left=housing_env_df_cleaned_re_ranked, right=educ_df_cleaned_re_ranked, how='outer', on='Zip Code')
+
+    final_joined_df = pd.merge(left=merge1, right=merge2, how='outer', on='Zip Code')
+    final_joined_df.to_csv(os.path.join(PROCESSED_DATA_DIR, 'all_ranked_metrics.csv'), index=False)
 
 
 main()
